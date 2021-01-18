7ce0531015127a59ccab14a716f360def7b30a1a
andreeanen <andreeanen@yahoo.com>
2021-01-18 15:04:23 +0100

    Update Pizzeria-API project to Core 5 and removes https Add a new project for the Pizzeria storage

--- Pizzeria-API/Pizzeria-Storage-API/Startup.cs
+++ Pizzeria-API/Pizzeria-Storage-API/Startup.cs
@@ -1,10 +1,17 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
-namespace Pizzeria_API
+namespace Pizzeria_Storage_API
 {
     public class Startup
     {
@@ -18,7 +25,12 @@ namespace Pizzeria_API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+
             services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Pizzeria_Storage_API", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -27,9 +39,9 @@ namespace Pizzeria_API
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Pizzeria_Storage_API v1"));
             }
-
-            //app.UseHttpsRedirection();
 
             app.UseRouting();
 
